32e21de11cec732b8abe751fb137a29d10560cd4
CarmAlex <a01198988@tec.mx>
2026-04-06 18:06:32 -0600

    Actualizar firebase.config.ts

--- app/firebase/firebase.config.ts
+++ app/firebase/firebase.config.ts
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJjmE1pUhPv8eG8JJAW8SPAl5azOtxQ8Q",
-  authDomain: "tc3005b-a01198988.firebaseapp.com",
-  projectId: "tc3005b-a01198988",
-  storageBucket: "tc3005b-a01198988.firebasestorage.app",
-  messagingSenderId: "883008148934",
-  appId: "1:883008148934:web:03f26a76fad2d87972cec7"
+  
 };
 
 // Initialize Firebase
